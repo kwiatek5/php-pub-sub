2d43e4b841271c43346115cc028dd311d6c41089
kwiatek5 <kwiatek5@vp.pl>
2013-09-08 12:30:11 +0200

    Closure to callable

--- PubSub.php
+++ PubSub.php
@@ -7,8 +7,11 @@ class PubSubException extends Exception {
 class PubSub {
 	private static $events = array();
 
-	public static function on($eventName, Closure $closure) {
-		self::$events[$eventName] = $closure;
+	public static function on($eventName, $callaback) {
+		if (!is_callable($callaback)) {
+			throw new PubSubException('Value is not callable!');
+		}
+		self::$events[$eventName] = $callaback;
 	}
 
 	public static function off($eventName) {
@@ -17,12 +20,16 @@ class PubSub {
 
 
 	public static function trigger($eventName) {
-		if (isset(self::$events[$eventName]) && self::$events[$eventName] instanceof Closure) {
-			$params = func_get_args();
-			array_shift($params);
-			return call_user_func_array(self::$events[$eventName], $params);
-		} else {
-			throw new PubSubException(sprintf('Event "%s" does not exists!', $eventName));
+		if (!isset(self::$events[$eventName])) {
+			throw new PubSubException('Event does not exists!');
 		}
+
+		if (!is_callable(self::$events[$eventName])) {
+			throw new PubSubException('Value is not callable!');
+		}
+
+		$params = func_get_args();
+		array_shift($params);
+		return call_user_func_array(self::$events[$eventName], $params);
 	}
 }
